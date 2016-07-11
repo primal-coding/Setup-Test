d0389775c14cbed4913a352cc1d7fc4cc505abc3
primal-coding <primal.coding@gmail.com>
2016-07-11 19:23:52 +0000

    Initial commit

---
+++ app/src/main/java/net/motion/in/stillness/esprit/setuptest/MainActivity.java
@@ -0,0 +1,17 @@
+package net.motion.in.stillness.esprit.setuptest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    private void testMethod(){
+        int tester = 1+1;
+    }
+}
